a1ac54fa8c40351f7c64d0e6ff59f4271e3985ce
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-28 14:20:33 -0400

    added an error catch on pingpong

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const ajaxButton = document.getElementById('ajax_button');
-  ajaxButton.addEventListener('click', function() {
+
+  const rootButton = document.getElementById('root_button');
+  rootButton.addEventListener('click', function() {
     request = axios.get('http://intro-ajax-api.herokuapp.com/');
+  });
+
+  const section3456 = document.getElementById('steps3456');
+
+  const pingPongButton = document.getElementById('ping_pong_button');
+  pingPongButton.addEventListener('click', function() {
+    request = axios.get('http://intro-ajax-api.herokuapp.com/pong')
+    .then(function(resp) {
+      console.log(resp.data);
+      section3456.append(resp.data);
+    })
+    .catch(function(error) {
+      section3456.append("That wasn't right, we'll try harder next time.")
+    });
   });
 });
