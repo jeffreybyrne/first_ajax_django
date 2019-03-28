6ada1948deafb331cc8053ad8473898aaf98aba0
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-28 13:46:17 -0400

    added a button that makes an ajax get request

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+  const ajaxButton = document.getElementById('ajax_button');
+  ajaxButton.addEventListener('click', function() {
+    request = axios.get('http://intro-ajax-api.herokuapp.com/');
+  });
 });
